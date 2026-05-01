2586a373197a1750a4f5d1fe0ccb1487980c537f
JesusAndrade <134724036+JesusAndrade2@users.noreply.github.com>
2026-04-30 23:24:12 -0500

    first version finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
-import { useState } from 'react'
+import confetti from "canvas-confetti";
+import { useEffect,useState } from 'react'
 
 import './App.css'
 
 function App() {
   const [mostrarMensaje, setMostrarMensaje] = useState(false);
-  return (
 
-    <div className="container">
+  return (
+    <>
+
+
+
+            <div className="container">
       <h1 className='title'> 
-        Feliz Cumpleaños
+        Feliz Cumpleaños 
+        <span>Tati</span>
         <span>🎉</span> <span>💕</span>
       </h1>
 
-      <h2>Para alguien muy especial 💖</h2>
+      <h2>Para alguien muy especial 💖 </h2>
 
       <p>
-        Hoy es un día increíble porque celebramos tu existencia ✨
+        Hoy es un día increíble porque celebramos tu existencia 💞
       </p>
 
-      <button className='btn-led' onClick={() => setMostrarMensaje(true)}>
-        Ver sorpresa 🎁
+      <button className='btn-led' onClick={
+        () => {
+          setMostrarMensaje(true);
+
+            confetti({
+              particleCount: 200,
+              spread: 120,
+              startVelocity: 40,
+              scalar: 1.2,
+        });
+        }
+      }
+        >
+        Ver sorpresa 😊
       </button>
 
       {mostrarMensaje && (
         <div className="mensaje">
           <p>
-            Gracias por estar en mi vida. Espero que este día esté lleno de
-            alegría, amor y momentos inolvidables 💕
+             Espero que este día esté lleno de alegría, amor y momentos inolvidables, un abrazo a la distancia y no olvides que aqui hay alguien que te quiere mucho {";)"} mucho mucho {"<3"}
           </p>
+          
         </div>
       )}
+
     </div>
+
+      
+    </>
   );
 }
 export default App
